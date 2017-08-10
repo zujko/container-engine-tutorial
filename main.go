e943d95218f0e07c9bed00510fca8a9cb6ab8625
zujko <peter@zujko.xyz>
2017-08-10 13:25:39 -0700

    Create simple time api

---
+++ main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/julienschmidt/httprouter"
+)
+
+type Time struct {
+	Timezone string    `json:"timezone"`
+	Time     time.Time `json:"time,string"`
+}
+
+func main() {
+	r := httprouter.New()
+
+	r.GET("/time/:country/:place", GetTime)
+	r.GET("/time", GetTime)
+
+	log.Fatal(http.ListenAndServe(":8080", r))
+}
+
+func GetTime(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+	country := p.ByName("country")
+	place := p.ByName("place")
+	var currentTime time.Time
+	tz := fmt.Sprintf("%s/%s", country, place)
+
+	if country == "" || place == "" {
+		currentTime = time.Now().UTC()
+	} else {
+		loc, err := time.LoadLocation(tz)
+		if err != nil {
+			http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
+			return
+		}
+		currentTime = time.Now().In(loc)
+	}
+	w.Header().Set("Content-Type", "application/json")
+
+	timeResponse, err := json.Marshal(&Time{tz, currentTime})
+	if err != nil {
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		return
+	}
+
+	w.Write(timeResponse)
+}
