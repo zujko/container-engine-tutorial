85a427485b96c2b24838009ec2e132607f5ab9a4
zujko <peter@zujko.xyz>
2017-08-17 13:00:40 -0700

    Add Dockerfile

--- main.go
+++ main.go
@@ -18,20 +18,21 @@ type Time struct {
 func main() {
 	r := httprouter.New()
 
-	r.GET("/time/:country/:place", GetTime)
+	r.GET("/time/:continent/:place", GetTime)
 	r.GET("/time", GetTime)
 
 	log.Fatal(http.ListenAndServe(":8080", r))
 }
 
 func GetTime(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
-	country := p.ByName("country")
+	country := p.ByName("continent")
 	place := p.ByName("place")
 	var currentTime time.Time
 	tz := fmt.Sprintf("%s/%s", country, place)
 
 	if country == "" || place == "" {
 		currentTime = time.Now().UTC()
+		tz = "UTC"
 	} else {
 		loc, err := time.LoadLocation(tz)
 		if err != nil {
